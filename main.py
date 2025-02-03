f61bce92eeceb93e580e29fb52e22bbbe75c0dd6
Tomáš Kučera <105876215+rodrickhmmm@users.noreply.github.com>
2025-02-03 11:22:01 +0100

    Pridal jsem main.py a nic vic lolololololollooollololo

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# importuješ moduly
+import pyautogui
+
+# KONFIGURACE BAREV 
+cervena = "\033[31;49;1m"
+zluta = "\033[33;49;1m"
+modra = '\033[34;49;1m'
+zelena = "\033[32;49;1m"
+bila = "\033[0m"
+cyan = "\x1b[36m"
+
+# main menu
+print(bila + "===", cyan + "ZAVBot", bila + "===")      
