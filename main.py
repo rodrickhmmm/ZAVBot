60a3a3200b3656d91c876c2c89eb93da8407d808
Rodrick_ <105876215+rodrickhmmm@users.noreply.github.com>
2025-02-20 17:31:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,57 @@
-
-# importuješ moduly
+from colorama import Fore
 import pyautogui
 
-# KONFIGURACE BAREV 
-cervena = "\033[31;49;1m"
-zluta = "\033[33;49;1m"
-modra = '\033[34;49;1m'
-zelena = "\033[32;49;1m"
-bila = "\033[0m"
-cyan = "\x1b[36m"
+def credits():
+    print("Naprogramoval Rodrick__")
+    print("https://github.com/Rodrickhmmm/ZAVBot")
 
-# main menu
-print(bila + "===", cyan + "ZAVBot", bila + "===")      
+typer():
+    print(Fore.RESET + "[1] Normální cvičení")
+    print("[2] Cvičení z papíru")
+    print("[3] Lenka kajaka")
+    print("[4] Padající písmena")
+    print("[4] Petiminutovky/desetiminutovky")
+    print("[4] Exit")
+    
+    try:
+        choose = int(input(Fore.CYAN + "Vyber: " + Fore.GREEN))
+    except ValueError:
+        print("tak jseš kokot?? Vyber číslo!!")
+        exit()
+    if choose > 5:
+        print("vyber od 1 do 4!!!!")
+        exit()
+    elif choose == 1:
+        
+    elif choose == 2:
+        
+    elif choose == 3:
+        
+    elif choose == 4:
+        mainmenu()
+
+def mainmenu():
+    print(Fore.CYAN + "--- ZAVBot ---")
+    print(Fore.RESET + "[1] Typer")
+    print("[2] Nastavení")
+    print("[3] Credits")
+    print("[4] Exit")
+    
+    try:
+        choose = int(input(Fore.CYAN + "Vyber: " + Fore.GREEN))
+    except ValueError:
+        print("tak jseš kokot?? Vyber číslo!!")
+        exit()
+    if choose > 5:
+        print("vyber od 1 do 4!!!!")
+        exit()
+    elif choose == 1:
+        typer()
+    elif choose == 2:
+        settings()
+    elif choose == 3:
+        credits()
+    elif choose == 4:
+        exit()
+
+mainmenu()
